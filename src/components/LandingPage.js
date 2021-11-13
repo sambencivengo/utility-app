b5762786a3c79029ceee1c611fb589248d55ca3b
Sam Bencivengo <sambencivengo@gmail.com>
2021-11-13 08:47:11 -0500

    Clean up

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,7 @@
 const LandingPage = () => {
 	const url = `api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
 
-	console.log(APIkey);
+	// console.log(APIkey);
 	return (
 		<div className="landing">
 			<h1>Landing Page</h1>
